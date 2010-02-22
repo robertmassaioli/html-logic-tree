d9d826388d6c67bcdaac43e190cd7698abf5f151
Robert Massaioli <robertmassaioli@gmail.com>
2010-02-22 12:58:34 +1100

    Created the init stages for the LogicTree

--- drawLogic.js
+++ drawLogic.js
@@ -1,13 +1,14 @@
 LogicTreeSettings = {
   nodeHeight : 2,
   nodeWidth : 1,
-  extraWidth : 2,
+  sideLines : true,
   nodeNameRequired : true
 }
 
-LogicTree = {
+LogicTreeHelper = {
   settings : LogicTreeSettings,
   isValid : function (tree) {
+    // TODO See if this function is nicer by starting with valid = false
     var valid = true;
 
     if (!tree.type) return false;
@@ -47,28 +48,82 @@ LogicTree = {
 
     return valid;
   },
-  getHeight : function (tree) {
+  calculateHeight : function (tree) {
     if (tree.type == "Val") {
-      return this.settings.nodeHeight; // default
+      tree.height = this.settings.nodeHeight; // default
     } else if (tree.value == "and") {
-      return Math.max(this.getHeight(tree.children[0]), this.getHeight(tree.children[1]));
+      tree.height = Math.max(this.calculateHeight(tree.children[0]), this.calculateHeight(tree.children[1]));
     } else if (tree.value == "or") {
-      return this.getHeight(tree.children[0]) + this.getHeight(tree.children[1]);
+      tree.height = this.calculateHeight(tree.children[0]) + this.calculateHeight(tree.children[1]);
+    } else {
+      tree.height = -1; // obviously an error
     }
-    return -100;  // TODO the isValid function should prevent this from happening
+    return tree.height;
   },
-  getWidth : function (tree) {
+  calculateWidth : function (tree) {
     if (tree.type == "Val") {
-      return this.settings.nodeWidth; // default
+      tree.width = this.settings.nodeWidth; // default
     } else if (tree.value == "and") {
-      return this.getWidth(tree.children[0]) + this.getWidth(tree.children[1]) + 1; // The +1 is the bar for the and
+      tree.width = this.calculateWidth(tree.children[0]) + this.calculateWidth(tree.children[1]) + 1; // The +1 is the bar for the and
     } else if (tree.value == "or") {
-      return Math.max(this.getWidth(tree.children[0]), this.getWidth(tree.children[1])) + 2; // The +2 is for bars in the Or
+      tree.width = Math.max(this.calculateWidth(tree.children[0]), this.calculateWidth(tree.children[1])) + 2; // The +2 is for bars in the Or
+    } else {
+      tree.width = -1; // obviously an error
     }
-    return -100;  // TODO the isValid function should prevent this from happening
+    return tree.width;
+  },
+  drawNode : function (tree) {
   }
 }
 
-function drawLogic (logic) {
+LogicTree = {
+  helper : LogicTreeHelper,
+  canvas : null,
+  context : null,
+  tree : null,
+  canvas_width : 0,
+  canvas_height : 0,
+  tiles_wide : 0,
+  tiles_high : 0,
+  tree_loaded : false,
+  error_message : null,
 
+  init : function (a_tree, a_canvas) {
+    tree_loaded = false;
+    if (!a_tree) { 
+      this.error_message = "init: Tree was null";
+      return false;
+    }
+    
+    if (!a_canvas) { 
+      this.error_message = "init: Canvas was null";
+      return false;
+    }
+    
+    if(!a_canvas.getContext) {
+      this.error_message = "init: canvas 'getContext' function not found. Maybe your browser does not support HTML5?";
+      return false;
+    }
+    
+    // error handling done, now create stuff
+    this.tree = a_tree;
+    this.canvas = a_canvas;
+
+    this.context = this.canvas.getContext('2d');
+    if(!this.helper.isValid(this.tree)) {
+      this.error_message = "init: Tree given contained invalid syntax.";
+      return false;
+    }
+
+    // canvas details
+    this.canvas_height = this.canvas.clientHeight;
+    this.canvas_width = this.canvas.clientWidth;
+
+    // tile details
+    this.tiles_wide = this.helper.calculateWidth(this.tree) + (this.helper.settings.sideLines ? 2 : 0);
+    this.tiles_high = this.helper.calculateHeight(this.tree);
+
+    this.tree_loaded = true;
+    return true;
+  }
 }
