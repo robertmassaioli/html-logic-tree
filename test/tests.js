de000ee9cddf27afbbe79b7c5d20bab09750a8ac
Robert Massaioli <robertmassaioli@gmail.com>
2010-02-23 11:09:30 +1100

    Updated Tests and TODO
    The todo file was updated to reflect the current state.
    The tests now number ten and they come with info.

--- test/tests.js
+++ test/tests.js
@@ -1,225 +1,261 @@
-// test for basic node
 var testData = new Array();
+var testDataInfo = new Array();
 
-testData.push ({
-        name: "node",
+testDataInfo.push("A Basic node.");
+testData.push({
+    name: "node",
+    type: "Val",
+    value: "false"
+});
+
+testDataInfo.push("A test for the And operator.");
+testData.push({
+    type: "Op",
+    value: "and",
+    child: [{
+        name: "A",
+        type: "Val",
+        value: "true"
+    },
+    {
+        name: "B",
         type: "Val",
         value: "false"
-    });
+    }]
+});
 
-// a test for the and operator
-testData.push ({
-        type: "Op",
-        value: "and",
-        child: [{
-            name: "A",
-            type: "Val",
-            value: "true"
+testDataInfo.push("same as two but the Or operator");
+testData.push({
+    type: "Op",
+    value: "or",
+    child: [{
+        name: "A",
+        type: "Val",
+        value: "true"
+    },
+    {
+        name: "B",
+        type: "Val",
+        value: "false"
+    }]
+});
+
+testDataInfo.push("same as two but the Or operator");
+testData.push({
+    "type": "Op",
+    "value": "and",
+    "child": [{
+        "type": "Op",
+        "value": "or",
+        "child": [{
+            "name": "A",
+            "type": "Val",
+            "value": "true"
         },
         {
-            name: "B",
-            type: "Val",
-            value: "false"
+            "name": "B",
+            "type": "Val",
+            "value": "false"
         }]
-    });
-
-// same as two but the Or operator
-testData.push ({
-        type: "Op",
-        value: "or",
-        child: [{
-            name: "A",
-            type: "Val",
-            value: "true"
+    },
+    {
+        "type": "Op",
+        "value": "or",
+        "child": [{
+            "name": "C",
+            "type": "Val",
+            "value": "true"
         },
         {
-            name: "B",
-            type: "Val",
-            value: "false"
+            "name": "D",
+            "type": "Val",
+            "value": "false"
         }]
-    });
+    }]
+});
 
-// same as two but the Or operator
-testData.push ({
+testDataInfo.push("An Or containing And's");
+testData.push({
+    "type": "Op",
+    "value": "or",
+    "child": [{
         "type": "Op",
         "value": "and",
+        "child": [{
+            "name": "A",
+            "type": "Val",
+            "value": "true"
+        },
+        {
+            "name": "B",
+            "type": "Val",
+            "value": "false"
+        }]
+    },
+    {
+        "type": "Op",
+        "value": "and",
+        "child": [{
+            "name": "C",
+            "type": "Val",
+            "value": "true"
+        },
+        {
+            "name": "D",
+            "type": "Val",
+            "value": "false"
+        }]
+    }]
+});
+
+testDataInfo.push("same as two but the Or operator");
+testData.push({
+    "type": "Op",
+    "value": "and",
+    "child": [{
+        "type": "Op",
+        "value": "or",
+        "child": [{
+            "name": "A",
+            "type": "Val",
+            "value": "true"
+        },
+        {
+            "name": "B",
+            "type": "Val",
+            "value": "false"
+        }]
+    },
+    {
+        "name": "C",
+        "type": "Val",
+        "value": "true"
+    }]
+});
+
+testDataInfo.push("This is two or's");
+testData.push({
+    "type": "Op",
+    "value": "or",
+    "child": [{
+        "type": "Op",
+        "value": "or",
+        "child": [{
+            "name": "A",
+            "type": "Val",
+            "value": "true"
+        },
+        {
+            "name": "B",
+            "type": "Val",
+            "value": "false"
+        }]
+    },
+    {
+        "name": "C",
+        "type": "Val",
+        "value": "true"
+    }]
+});
+
+testDataInfo.push("This is three Or's, just to be sure.");
+testData.push({
+    "type": "Op",
+    "value": "or",
+    "child": [{
+        "type": "Op",
+        "value": "or",
         "child": [{
             "type": "Op",
             "value": "or",
             "child": [{
                 "name": "A",
-                "type": "Val",
-                "value": "true"
-            },
-            {
-                "name": "B",
-                "type": "Val",
-                "value": "false"
-            }]
-        },
-        {
-            "type": "Op",
-            "value": "or",
-            "child": [{
-                "name": "C",
                 "type": "Val",
                 "value": "true"
             },
             {
                 "name": "D",
                 "type": "Val",
-                "value": "false"
+                "value": "true"
             }]
+        },
+        {
+            "name": "B",
+            "type": "Val",
+            "value": "false"
         }]
-    });
+    },
+    {
+        "name": "C",
+        "type": "Val",
+        "value": "true"
+    }]
+});
 
-testData.push ({
+testDataInfo.push("This is three And's, just to be sure.");
+testData.push({
+    "type": "Op",
+    "value": "and",
+    "child": [{
         "type": "Op",
-        "value": "or",
+        "value": "and",
         "child": [{
             "type": "Op",
             "value": "and",
             "child": [{
                 "name": "A",
-                "type": "Val",
-                "value": "true"
-            },
-            {
-                "name": "B",
-                "type": "Val",
-                "value": "false"
-            }]
-        },
-        {
-            "type": "Op",
-            "value": "and",
-            "child": [{
-                "name": "C",
                 "type": "Val",
                 "value": "true"
             },
             {
                 "name": "D",
                 "type": "Val",
-                "value": "false"
+                "value": "maybe"
             }]
+        },
+        {
+            "name": "B",
+            "type": "Val",
+            "value": "false"
         }]
-    });
+    },
+    {
+        "name": "C",
+        "type": "Val",
+        "value": "true"
+    }]
+});
 
-// same as two but the Or operator
-testData.push ({
+testDataInfo.push("Three ands but in reverse order to the last test.");
+testData.push({
+    "type": "Op",
+    "value": "and",
+    "child": [{
+        "name": "C",
+        "type": "Val",
+        "value": "true"
+    },
+    {
         "type": "Op",
         "value": "and",
         "child": [{
-            "type": "Op",
-            "value": "or",
-            "child": [{
-                "name": "A",
-                "type": "Val",
-                "value": "true"
-            },
-            {
-                "name": "B",
-                "type": "Val",
-                "value": "false"
-            }]
+            "name": "B",
+            "type": "Val",
+            "value": "false"
         },
         {
-            "name": "C",
-            "type": "Val",
-            "value": "true"
-        }]
-    });
-
-// This is two or's
-testData.push ({
-        "type": "Op",
-        "value": "or",
-        "child": [{
-            "type": "Op",
-            "value": "or",
-            "child": [{
-                "name": "A",
-                "type": "Val",
-                "value": "true"
-            },
-            {
-                "name": "B",
-                "type": "Val",
-                "value": "false"
-            }]
-        },
-        {
-            "name": "C",
-            "type": "Val",
-            "value": "true"
-        }]
-    });
-
-// This is three Or's, just to be sure.
-testData.push ({
-        "type": "Op",
-        "value": "or",
-        "child": [{
-            "type": "Op",
-            "value": "or",
-            "child": [{
-                "type": "Op",
-                "value": "or",
-                "child": [{
-                    "name": "A",
-                    "type": "Val",
-                    "value": "true"
-                },
-                {
-                    "name": "D",
-                    "type": "Val",
-                    "value": "true"
-                }]
-            },
-            {
-                "name": "B",
-                "type": "Val",
-                "value": "false"
-            }]
-        },
-        {
-            "name": "C",
-            "type": "Val",
-            "value": "true"
-        }]
-    });
-
-// This is three And's, just to be sure.
-testData.push ({
-        "type": "Op",
-        "value": "and",
-        "child": [{
             "type": "Op",
             "value": "and",
             "child": [{
-                "type": "Op",
-                "value": "and",
-                "child": [{
-                    "name": "A",
-                    "type": "Val",
-                    "value": "true"
-                },
-                {
-                    "name": "D",
-                    "type": "Val",
-                    "value": "maybe"
-                }]
+                "name": "A",
+                "type": "Val",
+                "value": "true"
             },
             {
-                "name": "B",
+                "name": "D",
                 "type": "Val",
-                "value": "false"
+                "value": "maybe"
             }]
-        },
-        {
-            "name": "C",
-            "type": "Val",
-            "value": "true"
         }]
-    });
+    }]
+});
